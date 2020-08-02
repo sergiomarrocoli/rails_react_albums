6a34f557b1ee7332fd94f5ad53047066402d082f
Sergio Marrocoli <smarrocoli@yozu.co.uk>
2020-08-02 13:01:45 +0100

    Added extra testing to albums controller spec to check body

--- spec/requests/api/v1/albums_request_spec.rb
+++ spec/requests/api/v1/albums_request_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe "Api::V1::Albums", type: :request do
 
   context 'index' do
     it 'returns an array of albums' do
-      expect_any_instance_of(AlbumsService).to receive(:albums_hash_array).once
+      expect_any_instance_of(AlbumsService)
+        .to receive(:albums_hash_array).once
+        .and_return("albums_hash_array")
       get("/api/v1/albums")
-      expect(response.status).to eq(200)   
+      
+      expect(response.status).to eq(200)
+      expect(response.body).to eq "albums_hash_array"   
     end
   end
 end
