78f0dd3af58957b8ef00bd6ca2fa91ef4e2a2d54
Sergio Marrocoli <smarrocoli@yozu.co.uk>
2020-08-02 14:22:47 +0100

    Added spec for user#show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def show
+    user = ApiService.new.user(params[:id].to_i)
+    render json: UserMapper.new.user_hash(user)
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
 
+  context 'show' do
+    it 'returns data for a single user' do
+      user_id = 1
+      expect_any_instance_of(ApiService)
+      .to receive(:user).with(user_id).once
+      .and_return("user_hash")
+
+      expect_any_instance_of(UserMapper)
+      .to receive(:user_hash).with("user_hash").once
+      .and_return("simple_user_hash")
+
+      get("/api/v1/users/#{user_id}")
+      expect(response.status).to eq(200)
+      expect(response.body).to eq "simple_user_hash"
+    end
+  end
 end
