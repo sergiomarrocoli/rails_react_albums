e6de477a69efd95e801c95c513e6ff4f417bde5c
Sergio Marrocoli <smarrocoli@yozu.co.uk>
2020-08-02 12:57:28 +0100

    Added index action to albums controller, spec and route

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,3 +1,5 @@
 class Api::V1::AlbumsController < ApplicationController
-  
+  def index
+    render json: AlbumsService.new.albums_hash_array
+  end
 end
--- spec/requests/api/v1/albums_request_spec.rb
+++ spec/requests/api/v1/albums_request_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Albums", type: :request do
 
+  context 'index' do
+    it 'returns an array of albums' do
+      expect_any_instance_of(AlbumsService).to receive(:albums_hash_array).once
+      get("/api/v1/albums")
+      expect(response.status).to eq(200)   
+    end
+  end
 end
